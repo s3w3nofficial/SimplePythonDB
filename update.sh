e5ed91519504a69f3c28be2cc6050a06a57aa718
Martin Urbanec <martin@urbanec.cz>
2017-05-18 11:06:19 +0000

    +update.sh

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo Stopping server
+test -e ~/run.pid && kill $(cat ~/run.pid)
+sleep 30
+echo Pulling from GitHub
+git pull
+echo Starting server again
+sleep 30
+python __init__.py
