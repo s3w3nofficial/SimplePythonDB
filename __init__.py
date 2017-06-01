9bc6d340f914f9cd069c80e71151576781bb7cf5
s3w3n <s3w3nofficial@gmail.com>
2017-06-01 14:20:31 +0200

    fix ident

--- __init__.py
+++ __init__.py
@@ -230,77 +230,76 @@ def insertIntoFiltered(data):
 
 		arguments_arg = {}
 		if len(args.split('=')) == 2:
-		        temp = args.split('=')
-		        arguments_arg[temp[0]] = temp[1]
-		else:   
-		        for arg in args:
-		                temp = arg.split('=')
-		                arguments_arg[temp[0]] = temp[1]
+			temp = args.split('=')
+			arguments_arg[temp[0]] = temp[1]
+		else:
+			for arg in args:
+				temp = arg.split('=')
+				arguments_arg[temp[0]] = temp[1]
 
 		arguments = {}
 		if len(args2.split('=')) == 2:
-		        temp = args2.split('=')
-		        arguments[temp[0]] = temp[1]
-		else:   
-		        for arg in args2:
-		                temp = arg.split('=')
-		                arguments[temp[0]] = temp[1]
+			temp = args2.split('=')
+			arguments[temp[0]] = temp[1]
+		else:
+			for arg in args2:
+				temp = arg.split('=')
+				arguments[temp[0]] = temp[1]
 
 		temp_args = []
-		rows = open("/home/s3w3n/Documents/DB/" + str(data), "r").readlines()
+		rows = open("/home/spdb/DB/" + str(data), "r").readlines()
 		temp_args = rows[0].replace('\n', '')
 		temp_args = temp_args.split(' ')
 
 
 		index = 0
 		for arg in temp_args:
-		        if arg == list(arguments.keys())[0]:
-		                break
-		        index += 1
+			if arg == list(arguments.keys())[0]:
+				break
+			index += 1
 
 		value = arguments[list(arguments.keys())[0]]
 
 		temp = ""
 		for arg in args:
-		for arg in args:
-        i = 0
-        rows = open("/home/s3w3n/Documents/DB/" + str(data), "r").readlines()
-        for col in rows[0].split(' '):
-                if arg in col:
-                        break
-                i += 1  
-                
-        rows.pop(0)
-        
-        rows_temp = []
-        rows_temp_index = []
-        
-        a = 0
-        for row in rows:
-                row = row.replace('\n', '')
-                cols = row.split()
-                if cols[index] == value:
-                        rows_temp.append(row)
-                        rows_temp_index.append(a+1)
-                a += 1  
-                
+			i = 0
+			rows = open("/home/spdb/DB/" + str(data), "r").readlines()
+			for col in rows[0].split(' '):
+				if arg in col:
+					break
+				i += 1
+
+			rows.pop(0)
+			
+			rows_temp = []
+			rows_temp_index = []
+
+			a = 0
+			for row in rows:
+				row = row.replace('\n', '')
+				cols = row.split()
+				if cols[index] == value:
+					rows_temp.append(row)
+					rows_temp_index.append(a+1)
+				a += 1
+
 		lines = []
 		i -= 1
 		value_arg = arguments_arg[list(arguments_arg.keys())[0]]
 		for row in rows_temp:
-		        cols = row.split(' ')
-		        if len(cols) > i + 2:
-		                cols[i] = value_arg + ","
-		        else:   
-		                cols[i] = value_arg
-		        lines.append(' '.join(cols))
+			cols = row.split(' ')
+			if len(cols) > i + 2:
+				cols[i] = value_arg + ","
+			else:
+				cols[i] = value_arg
+			lines.append(' '.join(cols))
 
 
-		a = 0
+		a = 0 
 		for index in rows_temp_index:
-		        replace_line("/home/s3w3n/Documents/DB/" + str(data), index, lines[a]+"\n")
-		        a += 1
-		return "succes"   
+			replace_line("/home/spdb/DB/" + str(data), index, lines[a]+"\n")
+			a += 1
+		return "succes"	
     else:
 		return "table doesnt exist"
 
